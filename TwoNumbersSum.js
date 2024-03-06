8ca3773a64b17b530d6328d4e4cff7cea898d22a
Shashwat Pathak <shashwatpathak18@gmail.com>
2024-03-06 17:17:29 +0530

    new file added

--- TwoNumbersSum.js
+++ TwoNumbersSum.js
@@ -1,19 +1,19 @@
 // Given a list of numbers and a number k, return whether
 // any two numbers from the list add up to k.
 
-// For example, given [10, 15, 3, 7] and k of 17, return 
+// For example, given [10, 15, 3, 7] and k of 17, return
 // true since 10 + 7 is 17.
 
 const findTarget = (arr, target) => {
-for(let i = 0 ; i < arr.length ; i++ ){
-    for(let j = i+1 ; j < arr.length ; j++){
-        if(arr[i] + arr[j] === target){
-            return true;
-        }
+  const compArr = [];
+  for (let elem of arr) {
+    if (compArr.includes(target - elem)) {
+      return true;
     }
-}
-}
+    compArr.push(elem);
+  }
+};
 
-const arr = [10,15,3,7]
+const arr = [10, 15, 3, 7];
 let target = 17;
-console.log(findTarget(arr,target));
+console.log(findTarget(arr, target));
